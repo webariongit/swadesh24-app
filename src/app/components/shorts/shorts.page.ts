5315fc7c921cdda84302072e125ace76d7276569
Subrata Sarkar <subratasarkar783@gmail.com>
2024-01-15 17:01:16 +0530

    short list load

--- src/app/components/shorts/shorts.page.ts
+++ src/app/components/shorts/shorts.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 import { apiRoutes } from 'src/app/constant/config';
 import { HttpService } from 'src/app/service/http-service/http.service';
 import Swiper from 'swiper';
@@ -9,10 +10,14 @@ import Swiper from 'swiper';
   styleUrls: ['./shorts.page.scss'],
 })
 export class ShortsPage implements OnInit {
-
+  shorts:any = [];
   shortList:any
   base_url:any;
   loader:boolean = false;
+  currentPage: number = 1;
+  totalPage:number = 1;
+  firstLoad:boolean = true;
+
   constructor(
     private httpService:HttpService,
     private router:Router,
@@ -27,22 +32,38 @@ export class ShortsPage implements OnInit {
   }
 
   getShortList(){
-    this.loader = true;
-    this.httpService.get(apiRoutes.shorts).subscribe({
-      next:(v:any) =>{
-        this.base_url =  v.base_path
-        this.shortList = v?.response?.data
-        this.loader = false;
-      },
-      error:(e:any)=>{
-        this.loader = false;
-        if (e.status == 401) {
-          localStorage.clear();
-          this.router.navigate(['login']); 
-        }
+    this.shortList = [];
+    if(this.currentPage <= this.totalPage){
+      if(this.firstLoad){
+        this.loader = true;
+      }else{
+        this.currentPage = Number(this.currentPage) + 1;
       }
-    })
+      let apiUrl = apiRoutes.shorts + '?page=' + this.currentPage
+      this.httpService.get(apiUrl).subscribe({
+        next:(v:any) =>{
+          this.base_url = v.base_path
+          this.firstLoad = false;
+          this.loader = false;
+          this.shortList = v.response?.data;
+          this.totalPage = v.response?.last_page;
+          for(var i=0; i < this.shortList?.length; i++){
+            this.shorts.push(this.shortList[i])
+          }
+          localStorage.setItem("shorts", JSON.stringify(this.shorts))
+        },
+        error:(e:any)=>{
+          this.loader = false;
+        }
+      })
+    }
   }
 
+  getMoreList(infiniteScroll:any){
+    this.getShortList()
+    setTimeout(() => {
+      (infiniteScroll as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
+  }
 
 }
